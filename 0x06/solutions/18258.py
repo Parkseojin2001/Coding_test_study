ed7d27601361b25cdb793275c6430d333977e27a
Parkseojin2001 <seojin0510612@gmail.com>
2026-03-06 21:08:52 +0900

    update 0x06 workbook code

--- 0x06/solutions/18258.py
+++ 0x06/solutions/18258.py
@@ -1,5 +1,65 @@
 # Authored by : 21011645
-# Co-authored by : -
-# http://boj.kr/****************
+# https://www.acmicpc.net/problem/18258
 import sys
-input = sys.stdin.readline
+from collections import deque
+
+
+def sys_input() -> str:
+    return sys.stdin.readline().rstrip()
+
+
+def push(n: int, queue: list[int]) -> None:
+    queue.append(n)
+
+
+def remove(queue: list[int]) -> int:
+    if not queue:
+        return -1
+    return queue.popleft()
+
+
+def size(queue: list[int]) -> int:
+    return len(queue)
+
+
+def empty(queue: list[int]) -> int:
+    if queue:
+        return 0
+    return 1
+
+
+def front(queue: list[int]) -> int:
+    if not queue:
+        return -1
+    return queue[0]
+
+
+def back(queue: list[int]) -> int:
+    if not queue:
+        return -1
+    return queue[-1]
+
+
+def main() -> None:
+    N = int(sys_input())
+    queue = deque()
+
+    for _ in range(N):
+        order = list(sys_input().split())
+
+        if order[0] == "push":
+            push(order[1], queue)
+        elif order[0] == "pop":
+            print(remove(queue))
+        elif order[0] == "size":
+            print(size(queue))
+        elif order[0] == "empty":
+            print(empty(queue))
+        elif order[0] == "front":
+            print(front(queue))
+        else:
+            print(back(queue))
+
+
+if __name__ == "__main__":
+    main()
--- 0x06/solutions/2164.py
+++ 0x06/solutions/2164.py
@@ -1,5 +1,29 @@
 # Authored by : 21011645
-# Co-authored by : -
-# http://boj.kr/****************
+# https://www.acmicpc.net/problem/2164
 import sys
-input = sys.stdin.readline
+from collections import deque
+
+
+def sys_input() -> str:
+    return sys.stdin.readline().rstrip()
+
+
+def solve(n: int) -> int:
+    cards = deque(range(1, n + 1))
+
+    while len(cards) > 1:
+        cards.popleft()
+        cards.rotate(-1)
+
+    return cards[0]
+
+
+def main() -> None:
+    N = int(sys_input())
+    answer: int = solve(N)
+
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
